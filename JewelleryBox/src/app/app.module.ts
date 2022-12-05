4ef1e93cd2492d674869159368020f04198d70a0
Erik Hayrelov <erik_man.united@abv.bg>
2022-12-05 04:19:03 +0200

    Added Components to app.component

--- JewelleryBox/src/app/app.module.ts
+++ JewelleryBox/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages//home/home.component';
+import { JewelsComponent } from './pages/jewels/jewels.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    JewelsComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
