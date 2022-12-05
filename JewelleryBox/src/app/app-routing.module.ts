ec0a8417559e752d35a0b5310a9557448e01639d
Erik Hayrelov <erik_man.united@abv.bg>
2022-12-05 04:18:26 +0200

    Added Routing

--- JewelleryBox/src/app/app-routing.module.ts
+++ JewelleryBox/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { JewelsComponent } from './pages/jewels/jewels.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'jewels',component:JewelsComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
