619c8d9a98efa0455d537aa581aaa0448712d2e6
Erik Hayrelov <erik_man.united@abv.bg>
2022-12-08 07:21:39 +0200

    Added Jewels sample data

---
+++ JewelleryBox/src/data.ts
@@ -0,0 +1,121 @@
+import { Jewel } from "./app/shared/models/jewel";
+
+export const sample_jewels: Jewel[] = [
+    {
+        "title": "Bridal Butterfly Crystal Gold Upper Arm Bracelet",
+        "id": "1",
+        "description": "Size (diameter) : 3.5\" One Size Fits Most (Stretchable)\nButterfly Szie: 1.75\" X 1\" (4.5 cm X 2.5 cm)\nGrab everyone's attention\nPerfect item for your wedding parties, special events, etc.\nMaterial : High Quality Clear Austrian Crystal Rhinestone, Gold Plated Metal\nCan be worn as a bridal and prom accessory\nWonderful gift for a loved one (or yourself!)",
+        "price": 17.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/butterfly_gold_01.jpg",
+        "tags": [
+            "Bridal Armlet",
+            "Bridal Bracelet",
+            "Upper Arm Bracelet"
+        ]
+    },
+    {
+        "title": "10 Row Crystal Rhinestone Bridal Wedding Bracelet",
+        "id": "2",
+        "description": "Size: One Size Fits Most (Stretchable)\nDiameter: Approx. 2.1 inches (52 mm)\nCan be Worn as Bangle Bracelet or Upper Arm Bracelet\nGrab everyone's attention\nPerfect item for your wedding parties, special events, etc.\nMaterial : High Quality Clear Austrian Crystal Rhinestone, Silver Plated Metal\nCan be worn as a Wedding and Fashion accessory",
+        "price": 41.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/a017_02.jpg",
+        "tags": [
+            "10 Row Armlet",
+            "Bridal Armlet",
+            "Rhinestone Armlet",
+            "Upper Arm Bracelet",
+            "Wedding Armlet"
+        ]
+    },
+    {
+        "title": "Hearts Bridal Wedding Stretch Crystal Gold Bracelet",
+        "id": "3",
+        "description": "Length: 6\" stretch to 8\" (Stretchable)\nWith elastic cord inside\nWidth: 15 mm\nBrand New Material: High Quality Clear Austrian Crystal Rhinestone, Gold Plated Metal\nCan be worn as a bridal and fashion accessory\nPerfect item for your wedding parties, special events, etc.\nWonderful gift for a loved one (or yourself!)",
+        "price": 14.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/b1037_01.jpg",
+        "tags": [
+            "Bridal Bracelet",
+            "Bridal jewelry",
+            "Bridesmaid Jewelry",
+            "Crystal Bracelet",
+            "Rhinestone Bracelet",
+            "Wedding Favors",
+            "Wedding jewelry"
+        ]
+    },
+    {
+        "title": "Silver & Gold Tone Cubic Zirconia Studs 18k White Gold Plated Mens Earrings",
+        "id": "4",
+        "description": "Size: (wide 7mm; diameter 14mm)\nPiercing Earrings\nIt looks cool and fashionable\nBrand New Product & Best Selling Material: 18k white gold plated metal;\nCubic Zirconia Studs",
+        "price": 9.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/me288_g.jpg",
+        "tags": [
+            "Mens Earrings",
+            "Silver Earrings",
+            "Gold Earrings",
+            "White Gold Earrings"
+        ]
+    },
+    {
+        "title": "Cubic Zirconia Studs 18K White Gold Plated Mens Earrings",
+        "id": "5",
+        "description": "Size (Diameter): 14 mm\nWidth: 3 mm\nWeight: 1.90 grams / each\nPiercing Earrings\nModern and Fashionable\nMaterial: 18K white gold plated metal & Cubic Zirconia Studs",
+        "price": 8.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/me263_g.jpg",
+        "tags": [
+            "Mens Earrings",
+            "White Gold Earrings"
+        ]
+    },
+    {
+        "title": "4\" Sparkling Crystal Rhinestone Dangle Earrings",
+        "id": "6",
+        "description": "Size: 4\" ( 10 cm)\nPierced Earrings\nGrab everyone's attention\nMaterial : High Quality Clear Austrian Crystal Rhinestone, Silver Plated Metal\nCan be worn as a bridal and fashion accessory\nWonderful gift for a loved one (or yourself!)",
+        "price": 14.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/e1020_g.jpg",
+        "tags": [
+            "Bridal Earrings",
+            "Bridal Jewelry",
+            "Crystal Earrings",
+            "Dangling Rhinestone Earrings",
+            "Wedding Earrings",
+            "Wedding Jewelry"
+        ]
+    },
+    {
+        "title": "Bridal Wedding 4 Row Crystal Rhinestone Flat Choker",
+        "id": "7",
+        "description": "Neck Size : 12\" - 14\" (30 cm - 35.5 cm) Adjustable\nWidth: 1.4 cm\nMaterial : High Quality Clear Austrian Crystal Rhinestone, Silver Plated Metal\nWonderful gift for a loved one (or yourself!)\nCan be worn as a bridal accessory",
+        "price": 32.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/c042_1.jpg",
+        "tags": [
+            "Bridal Choker",
+            "Wedding Choker",
+            "Crystal Choker"
+        ]
+    },
+    {
+        "title": "Bridal Wedding Butterfly Rhinestone Silver Choker",
+        "id": "8",
+        "description": "Circumference : 11\" (28 cm) But Extendable With Chain\nButterfly Size : 1.25\" X 1.25\" (3 cm X 3 cm)\nChain Size: 6\" (15 cm)\nOpen Backing Allowing A \"One Size Fits All\"\nMaterial : High Quality Clear Austrian Crystal Rhinestone, Silver Plated Metal",
+        "price": 32.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/C041_2.jpg",
+        "tags": [
+            "Bridal Choker",
+            "Wedding Choker",
+            "Silver Choker"
+        ]
+    },
+    {
+        "title": "Vintage Style White Shell Pearl w/ Swarovski Crystal Necklace",
+        "id": "9",
+        "description": "Necklace Size : 15\" - 17.5\" (38 cm - 44.5 cm)\nAdjustable Width: 1.4 cm\nPearl Diameter: 0.7 cm\nBrand New Pearl Colour: White\nPearls: Made of Shell Powder (not plastic bead)\nMetal : White Gold Plated Metal\nStones: Swarovski Crystals\nGuaranteed Good Quality",
+        "price": 32.99,
+        "imageUrl": "https://cdn.shopify.com/s/files/1/2481/1410/products/c033_01.jpg",
+        "tags": [
+            "Vintage Style Necklace",
+            "Vintage Style Pearl Necklace",
+            "White Pearl Necklace"
+        ]
+    }
+]
