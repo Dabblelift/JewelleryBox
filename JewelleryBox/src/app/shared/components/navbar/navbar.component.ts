3503b3aabe09904802edcf8f2044a57e3dbbfdc5
Erik Hayrelov <erik_man.united@abv.bg>
2022-12-17 02:15:19 +0200

    Fixed a bug with pathing on navbar component

--- JewelleryBox/src/app/shared/components/navbar/navbar.component.ts
+++ JewelleryBox/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { UserService } from 'src/app/services/user.service';
-import { User } from '../models/User';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-navbar',
